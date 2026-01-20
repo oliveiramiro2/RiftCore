c35fc561176faf59547143b9cf3c96ef72c85cb7
Ramiro <ramiromares95@gmail.com>
2026-01-20 14:26:38 -0300

    added some adjust on background effect, to UI to make it more visible and make boss damage

--- Assets/Script/UI/BackgroundAWArena.cs
+++ Assets/Script/UI/BackgroundAWArena.cs
@@ -29,12 +29,25 @@ public class BackgroundAWArena : MonoBehaviour
         {
             t += Time.deltaTime / 5f;
 
-            float eased = TemporalMath.SinPulse(t, 0.02f);
+            float eased = TemporalMath.EaseIn(t);
             material.SetFloat("_DistortionAmount", eased * distortion);
             yield return null;
         }
 
-        //material.SetFloat("_DistortionAmount", distortion);
+
+        yield return new WaitForSeconds(0.25f);
+
+        t = 0f;
+
+
+        while (t < 1f)
+        {
+            t += Time.deltaTime / 5f;
+
+            float eased = TemporalMath.SinPulse(t, 0.02f);
+            material.SetFloat("_DissolveAmount", eased * distortion);
+            yield return null;
+        }
 
         yield return new WaitForSeconds(0.25f);
 
@@ -42,11 +55,23 @@ public class BackgroundAWArena : MonoBehaviour
         {
             t -= Time.deltaTime / 5f;
 
-            float eased = TemporalMath.SinPulse(t, 0.02f);
+            float eased = TemporalMath.SinPulse(t, 0.03f);
+            material.SetFloat("_DissolveAmount", eased * distortion);
+            yield return null;
+        }
+
+        yield return new WaitForSeconds(0.25f);
+
+        t = 1f;
+
+        while (t > 0f)
+        {
+            t -= Time.deltaTime / 5f;
+
+            float eased = TemporalMath.EaseInOut(t);
             material.SetFloat("_DistortionAmount", eased * distortion);
             yield return null;
         }
-        //material.SetFloat("_DistortionStrength", 0f);
 
         yield return new WaitForSeconds(0.5f);
 
