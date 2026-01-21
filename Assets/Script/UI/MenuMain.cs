bc7cf498a198838b6e18c9a71487cbc063795080
Ramiro <ramiromares95@gmail.com>
2026-01-21 03:55:37 -0300

    added some adjust to up aw battle to itch.io

--- Assets/Script/UI/MenuMain.cs
+++ Assets/Script/UI/MenuMain.cs
@@ -5,15 +5,27 @@ using UnityEngine.InputSystem;
 
 public class MenuMain : MonoBehaviour
 {
+    [SerializeField] private Image chooseEnemyImage;
+
     void Start()
     {
         InputSystem.actions.FindActionMap("Player").Disable();
         InputSystem.actions.FindActionMap("UI").Enable();
     }
 
-    public void StartGame()
+    public void StartGameTet()
     {
         SceneManager.LoadScene("TETArena");
+    }
+
+    public void StartGameAW()
+    {
+        SceneManager.LoadScene("AWArena");
+    }
+
+    public void ChooseEnemy()
+    {
+        chooseEnemyImage.gameObject.SetActive(true);
     }
 
     public void QuitGame()
