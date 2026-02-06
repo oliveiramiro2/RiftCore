e8e516c49c043c58a8313d245e7b5372d0cded77
Ramiro <ramiromares95@gmail.com>
2026-02-06 15:11:52 -0300

    added events to play SFX

---
+++ Assets/Script/ToxicSlime/Managers/ToxicSlimeSFXManager.cs
@@ -0,0 +1,66 @@
+using UnityEngine;
+
+public class ToxicSlimeSFXManager : MonoBehaviour
+{
+  public SoundData death;
+  public SoundData projectilExplosion;
+  public SoundData hurt;
+  public SoundData jumpImpact;
+  public SoundData phase2;
+  public SoundData projectilLand;
+  public SoundData rainFall;
+  public SoundData regular;
+  public SoundData vomit;
+  public SoundData rolling;
+
+  public void PlayDeath()
+  {
+    death.Play();
+  }
+  public void PlayProjectilExplosion()
+  {
+    projectilExplosion.Play();
+  }
+
+  public void PlayHurt()
+  {
+    hurt.Play();
+  }
+
+  public void PlayJumpImpact()
+  {
+    jumpImpact.Play();
+  }
+
+  public void PlayPhase2()
+  {
+    phase2.Play();
+  }
+
+  public void PlayProjectilLand()
+  {
+    projectilLand.Play();
+  }
+
+  public void PlayRainFall()
+  {
+    rainFall.Play();
+  }
+
+  public void PlayRegular()
+  {
+    regular.Play();
+  }
+
+  public void PlayVomit()
+  {
+    vomit.Play();
+  }
+
+  public void PlayRolling()
+  {
+    rolling.Play();
+  }
+
+
+}
--- Assets/Script/ToxicSlime/Utils/ToxicSlimeEventsManager.cs
+++ Assets/Script/ToxicSlime/Utils/ToxicSlimeEventsManager.cs
@@ -5,4 +5,14 @@ public class ToxicSlimeEventsManager : ScriptableObject
 {
   public GameEvent OnToxicRainStart;
   public GameEvent OnToxicRainCloundAppear;
+  public GameEvent OnToxicDeath;
+  public GameEvent OnToxicHurt;
+  public GameEvent OnToxicPhase2;
+  public GameEvent OnToxicJumpImpact;
+  public GameEvent OnToxicProjectilLand;
+  public GameEvent OnToxicProjectilExplosion;
+  public GameEvent OnToxicRoll;
+  public GameEvent OnToxicVomit;
+  public GameEvent OnToxicRegular;
+  public GameEvent OnToxicRainFall;
 }
