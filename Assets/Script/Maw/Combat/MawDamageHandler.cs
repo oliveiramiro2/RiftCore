e4a18a66b446f4ab3fc2dbae44dee54b1e5caa3d
Ramiro <ramiromares95@gmail.com>
2026-03-13 12:55:12 -0300

    added hurtbox and contact hitbox to boss and buff 50% damage in your sword and 60% in your dash

---
+++ Assets/Script/Maw/Combat/MawDamageHandler.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class MawDamageHandler : DamageHandlerBase<MawController>
+{
+  private bool phase1 = true;
+
+  protected override void Awake()
+  {
+    base.Awake();
+  }
+
+  public override void TakeDamage(int damage, Vector2 hitDirection, float knockbackForce)
+  {
+    //controller.Events.Hurt.Raise();
+    base.TakeDamage(damage, hitDirection, knockbackForce);
+
+    if (isDead) return;
+    if (controller.Phase2() && phase1)
+    {
+      phase1 = false;
+      //controller.Events.Phase2.Raise();
+    }
+  }
+
+  protected override void Die()
+  {
+    controller.MarkAsDead();
+    base.Die();
+    //controller.Events.Death.Raise();
+    isDead = true;
+    Destroy(gameObject, 3f);
+  }
+}
